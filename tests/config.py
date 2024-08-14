dc7e2b7cba1234bd1997989ebc44b9f2023244f4
WD <me@wangdi.ink>
2024-08-14 10:18:45 +0800

    ci: use local db instead of cloud db (#55)

--- tests/config.py
+++ tests/config.py
@@ -2,7 +2,8 @@ import os
 
 
 class TestConfig:
-    TIDB_HOST = os.getenv("TEST_TIDB_HOST", "")
+    TIDB_HOST = os.getenv("TEST_TIDB_HOST", "127.0.0.1")
     TIDB_USER = os.getenv("TEST_TIDB_USER", "root")
     TIDB_PASSWORD = os.getenv("TEST_TIDB_PASSWORD", "")
     TIDB_PORT = int(os.getenv("TEST_TIDB_PORT", "4000"))
+    TIDB_SSL = os.getenv("TEST_TIDB_SSL", "false").lower() in ["true", "1"]
