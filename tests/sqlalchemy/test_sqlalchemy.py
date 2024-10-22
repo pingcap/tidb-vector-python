d6a792d18130c96c20d62f8c5cb25deb8a5dac2a
JaySon-Huang <tshent@qq.com>
2024-10-22 12:57:34 +0800

    Add simple test
    Signed-off-by: JaySon-Huang <tshent@qq.com>

--- tests/sqlalchemy/test_sqlalchemy.py
+++ tests/sqlalchemy/test_sqlalchemy.py
@@ -1,14 +1,17 @@
 import pytest
 import numpy as np
+import sqlalchemy
 from sqlalchemy import URL, create_engine, Column, Integer, select
 from sqlalchemy.orm import declarative_base, sessionmaker
 from sqlalchemy.exc import OperationalError
 from tidb_vector.sqlalchemy import VectorType
+import tidb_vector.sqlalchemy as sqlalchemy_tidb
 from ..config import TestConfig
 
 
 db_url = URL(
-    "mysql+pymysql",
+    #"mysql+pymysql",
+    "tidb+mysqldb",
     username=TestConfig.TIDB_USER,
     password=TestConfig.TIDB_PASSWORD,
     host=TestConfig.TIDB_HOST,
@@ -303,3 +306,71 @@ class TestSQLAlchemyWithDifferentDimensions:
             )
             assert len(items) == 2
             assert items[1].distance == -14.0
+
+class TestSQLAlchemyDDL:
+    def setup_class(self):
+        Item2Model.__table__.drop(bind=engine, checkfirst=True)
+        Item2Model.__table__.create(bind=engine)
+
+    def teardown_class(self):
+        Item2Model.__table__.drop(bind=engine, checkfirst=True)
+
+    def setup_method(self):
+        with Session() as session:
+            session.query(Item2Model).delete()
+            session.commit()
+
+    def test_l2_distance(self):
+        with Session() as session:
+            # indexes
+            l2_index = sqlalchemy_tidb.ddl.VectorIndex(
+                'idx_embedding_l2',
+                sqlalchemy.func.vec_l2_distance(Item2Model.__table__.c.embedding),
+            )
+            l2_index.create(engine)
+            cos_index = sqlalchemy_tidb.ddl.VectorIndex(
+                'idx_embedding_cos',
+                sqlalchemy.func.vec_cosine_distance(Item2Model.__table__.c.embedding),
+            )
+            cos_index.create(engine)
+
+            item1 = Item2Model(embedding=[1, 2, 3])
+            item2 = Item2Model(embedding=[1, 2, 3.2])
+            session.add_all([item1, item2])
+            session.commit()
+
+            # l2 distance
+            result_l2 = session.scalars(
+                select(Item2Model).filter(
+                    Item2Model.embedding.l2_distance([1, 2, 3.1]) < 0.2
+                )
+            ).all()
+            assert len(result_l2) == 2
+
+            distance_l2 = Item2Model.embedding.l2_distance([1, 2, 3])
+            items_l2 = (
+                session.query(Item2Model.id, distance_l2.label("distance"))
+                .order_by(distance_l2)
+                .limit(5)
+                .all()
+            )
+            assert len(items_l2) == 2
+            assert items_l2[0].distance == 0.0
+
+            # cosine distance
+            result_cos = session.scalars(
+                select(Item2Model).filter(
+                    Item2Model.embedding.cosine_distance([1, 2, 3.1]) < 0.2
+                )
+            ).all()
+            assert len(result_cos) == 2
+
+            distance_cos = Item2Model.embedding.cosine_distance([1, 2, 3])
+            items_cos = (
+                session.query(Item2Model.id, distance_cos.label("distance"))
+                .order_by(distance_cos)
+                .limit(5)
+                .all()
+            )
+            assert len(items_cos) == 2
+            assert items_cos[0].distance == 0.0
