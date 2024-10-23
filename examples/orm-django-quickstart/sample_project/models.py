7277977abb06d340a4ac325127525180459ac02f
JaySon-Huang <tshent@qq.com>
2024-10-23 18:43:23 +0800

    Update comments
    Signed-off-by: JaySon-Huang <tshent@qq.com>

--- examples/orm-django-quickstart/sample_project/models.py
+++ examples/orm-django-quickstart/sample_project/models.py
@@ -4,10 +4,6 @@ from django_tidb.fields.vector import VectorField, VectorIndex, CosineDistance, 
 
 class Document(models.Model):
     content = models.TextField()
-    # Note:
-    #   - Using comment to add hnsw index is a temporary solution. In the future it will use `CREATE INDEX` syntax.
-    #   - Currently the HNSW index cannot be changed after the table has been created.
-    #   - Only Django >= 4.2 supports `db_comment`.
     embedding = VectorField(dimensions=3)
     class Meta:
         indexes = [
