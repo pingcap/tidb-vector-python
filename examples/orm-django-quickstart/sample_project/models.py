d34d3b9d20fff93247f5bc5e3a0ba397397462de
JaySon <tshent@qq.com>
2024-11-05 18:02:25 +0800

    Support building vector index with django_tidb>=5.0.1 (#64)

--- examples/orm-django-quickstart/sample_project/models.py
+++ examples/orm-django-quickstart/sample_project/models.py
@@ -1,16 +1,12 @@
 from django.db import models
-from django_tidb.fields.vector import VectorField
+from django_tidb.fields.vector import VectorField, VectorIndex, CosineDistance, L2Distance
 
 
 class Document(models.Model):
     content = models.TextField()
     embedding = VectorField(dimensions=3)
-
-
-class DocumentWithIndex(models.Model):
-    content = models.TextField()
-    # Note:
-    #   - Using comment to add hnsw index is a temporary solution. In the future it will use `CREATE INDEX` syntax.
-    #   - Currently the HNSW index cannot be changed after the table has been created.
-    #   - Only Django >= 4.2 supports `db_comment`.
-    embedding = VectorField(dimensions=3, db_comment="hnsw(distance=cosine)")
+    class Meta:
+        indexes = [
+            VectorIndex(L2Distance("embedding"), name='idx_l2'),
+            VectorIndex(CosineDistance("embedding"), name='idx_cos'),
+        ]
